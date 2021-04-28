0317200cd7b133b87578c90e548c10559c3b75dd
JPSS14 <jp_scarabelli@hotmail.com>
2021-04-27 23:52:34 -0300

    react-bank UPDATE 1: Added style and test icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,21 @@
 import style from './Header.module.scss';
-
+import Link from 'next/link';
+import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
 
 export function Header() {
     return (
         <header className={style.mainHeader}>
-            <h1 className={style.logo}><a href="/">MyPortfolio</a></h1>
+            <h1 className={style.logo}><Link href="/"><a>MyPortfolio</a></Link></h1>
             <nav className={style.mainHeaderNav}>
                 <ul>
                     <li><a className="/icon-linkedin2" href="https://www.linkedin.com/in/jo%C3%A3o-pedro-s-silva/"></a></li>
-                    <li><a className="/icon-github" href="https://github.com/JPSS14"></a></li>
+                    <li><a href="https://github.com/JPSS14">
+                        <img className={style.git} src="/github-1.svg" alt="Github"/>
+                    </a></li>
                 </ul>
             </nav>
+            
         </header>
+        
     );
 }
