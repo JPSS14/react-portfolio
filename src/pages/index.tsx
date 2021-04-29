ea3b478a1d63291cd0380214182a75683a47ef9b
JPSS14 <jp_scarabelli@hotmail.com>
2021-04-28 23:10:27 -0300

    react-portfolio UPDATE 3: Added profile in main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Header } from '../components/Header'
+import style from '../styles/main.module.scss';
 
 export default function Home() {
   return (
@@ -8,9 +9,28 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
+      <main className={style.main}>
+        <div className={style.mainContent}>
 
-      <main>
-        <Header />
+          <div className={style.mainProfile}>
+            <div className={style.mainProfileContent}>
+              <div className={style.imgContainer}>
+                <img src="/jp.png" alt="João Pedro Scarabelli Silva" />
+              </div>
+              <div className={style.presentation}>
+                <p>João Pedro Scarabelli Silva</p>
+                <p>Desenvolvedor Web</p> 
+                <p>PHP</p> 
+                <p>JavaScript</p> 
+                <p>TypeScript</p>
+                <p>React</p> 
+                <p>Next JS</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </main>
     </>
   )
