0ce33d4a2811cca82b3d2a1caf76a565f5d1b587
JPSS14 <jp_scarabelli@hotmail.com>
2021-05-13 18:50:38 -0300

    react-portfolio UPDATE 13: Added certificateType function in certificate page

--- src/components/Certificates/index.tsx
+++ src/components/Certificates/index.tsx
@@ -1,7 +1,24 @@
+import certificates from '../../../certificate.json';
 import style from './Certificates.module.scss';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export function Certificates() {
+    const [certificateList, setCertificateList] = useState([]);
+    const [category, setCategory] = useState("front-end");
+
+    useEffect(() => {
+        setCertificateList(certificates.filter(certificates => certificates.destaque === "1"));
+    }, []);
+
+    function certificateType(type:string){
+        switch (type){
+            case "destaque": setCertificateList(certificates.filter(certificates => certificates.destaque === "1")); break;
+            case "front-end": setCertificateList(certificates.filter(certificates => certificates.categoria === "front-end" && certificates.index === "1")); break;
+            case "back-end": setCertificateList(certificates.filter(certificates => certificates.categoria === "back-end" && certificates.index === "1")); break;
+            case "outros": setCertificateList(certificates.filter(certificates => certificates.categoria === "outros" && certificates.index === "1")); break;
+        }
+    }
     return (
         <div className={style.mainCertificates}>
             <header>
@@ -10,15 +27,27 @@ export function Certificates() {
             </header>
 
             <div className={style.certificatesMenu}>
-                <button type="button">Destaques</button>
-                <button type="button">Front-end</button>
-                <button type="button">Back-end</button>
-                <button type="button">Outros</button>
+                <button type="button" onClick={() => certificateType("destaque")}>Destaques</button>
+                <button type="button" onClick={() => certificateType("front-end")}>Front-end</button>
+                <button type="button" onClick={() => certificateType("back-end")}>Back-end</button>
+                <button type="button" onClick={() => certificateType("outros")}>Outros</button>
             </div>
 
             <section className={style.certificatesSection}>
 
-                <article className={style.certificateArticle}>
+                {(category === "front-end") ? (
+                    certificateList.map((item, key) => (
+                        <article className={style.certificateArticle} key={key}>
+                            <div className={style.imgCertificate}>
+                                <img src={item.imgUrl} alt={item.nome} />
+                            </div>
+                        </article>
+                    ))
+                ) : (<></>)
+                }
+
+
+                {/* <article className={style.certificateArticle}>
                     <div className={style.imgCertificate}>
                         <img src="/certificado-webdesign-em-producao.png" alt="Certificado" />
                     </div>
@@ -32,7 +61,7 @@ export function Certificates() {
                     <div className={style.imgCertificate}>
                         <img src="/dio.png" alt="Certificado" />
                     </div>
-                </article>
+                </article> */}
             </section>
 
             <button className={style.readMore}>Ver todos</button>
