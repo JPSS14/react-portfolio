87f15ea3c8fddd22229281c1889dd42fa93d2009
JPSS14 <jp_scarabelli@hotmail.com>
2021-05-15 19:35:00 -0300

    react-portfolio UPDATE 17: Remove static projects articles and added projects data on projects.JSON

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import projects from '../../../projects.json';
 
 export function Projects() {
-    const react = projects.filter(projects => projects.categoria === "react");
+    const react = projects.filter(projects => projects.destaque === "1");
     return (
         <div className={style.mainProjects}>
             <header>
@@ -23,7 +23,7 @@ export function Projects() {
                     <article className={style.projectArticle} key={key}>
                         <header>
                             <h2>{item.nome}</h2>
-                            <p>Status: <span className={item.status === "Construção" ? style.building : ''}>{item.status}</span></p>
+                            <p>Status: <span className={item.status === "Construção" ? style.building : item.status === "Completo" ? style.complete : style.otmz}>{item.status}</span></p>
                             <p>Tipo: {item.tipo}</p>
                             <div className={style.articleTecnologies}>
                                 <ul>
@@ -47,105 +47,7 @@ export function Projects() {
                     </article>
                 ))}
 
-                <article className={style.projectArticle}>
-                    <header>
-                        <h2>React Bank</h2>
-                        <p>Status: <span className={style.building}>Construção</span></p>
-                        <p>Tipo: Pessoal</p>
-                        <div className={style.articleTecnologies}>
-                            <ul>
-                                <li>ReactJS</li>
-                                <li>NextJS</li>
-                                <li>TypeScript</li>
-                                <li>JavaScript</li>
-                                <li>SASS</li>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                            </ul>
-                        </div>
-                    </header>
-                    <div className={style.articleImgContainer}>
-                        <img src="/react-bank-count.png" alt="React Bank" />
-                    </div>
-                    <div className={style.articleContent}>
-                        <p className={style.title}>Introdução</p>
-                        <div className={style.text}>
-                            <p>React Bank é um simulador de banco, onde busco testar o uso do Sass e treinar o desenvolvimento de interfaces, além de enfatizar o uso de React, Next js e seus benefícios.</p>
-                            <p>Durante a criação da interface percebi que era possível implementar diversas funcionalidades e regras de negócio, todas a nivel de front-end.</p>
-                        </div>
-                        <Link href="/react-bank">
-                            <a className={style.link}>Saiba mais</a>
-                        </Link>
-
-                    </div>
-                </article>
-
-                <article className={style.projectArticle}>
-                    <header>
-                        <h2>React Bank</h2>
-                        <p>Status: <span className={style.building}>Construção</span></p>
-                        <p>Tipo: Pessoal</p>
-                        <div className={style.articleTecnologies}>
-                            <ul>
-                                <li>ReactJS</li>
-                                <li>NextJS</li>
-                                <li>TypeScript</li>
-                                <li>JavaScript</li>
-                                <li>SASS</li>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                            </ul>
-                        </div>
-                    </header>
-                    <div className={style.articleImgContainer}>
-                        <img src="/f1-react-img1.png" alt="React Bank" />
-                    </div>
-                    <div className={style.articleContent}>
-                        <p className={style.title}>Introdução</p>
-                        <div className={style.text}>
-                            <p>React Bank é um simulador de banco, onde busco testar o uso do Sass e treinar o desenvolvimento de interfaces, além de enfatizar o uso de React, Next js e seus benefícios.</p>
-                            <p>Durante a criação da interface percebi que era possível implementar diversas funcionalidades e regras de negócio, todas a nivel de front-end.</p>
-                        </div>
-                        <Link href="/react-bank">
-                            <a className={style.link}>Saiba mais</a>
-                        </Link>
-
-                    </div>
-                </article>
-
-                <article className={style.projectArticle}>
-                    <header>
-                        <h2>React Bank</h2>
-                        <p>Status: <span className={style.building}>Construção</span></p>
-                        <p>Tipo: Pessoal</p>
-                        <div className={style.articleTecnologies}>
-                            <ul>
-                                <li>ReactJS</li>
-                                <li>NextJS</li>
-                                <li>TypeScript</li>
-                                <li>JavaScript</li>
-                                <li>SASS</li>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                            </ul>
-                        </div>
-                    </header>
-                    <div className={style.articleImgContainer}>
-                        <img src="/buy-cars-front1.png" alt="React Bank" />
-                    </div>
-                    <div className={style.articleContent}>
-                        <p className={style.title}>Introdução</p>
-                        <div className={style.text}>
-                            <p>React Bank é um simulador de banco, onde busco testar o uso do Sass e treinar o desenvolvimento de interfaces, além de enfatizar o uso de React, Next js e seus benefícios.</p>
-                            <p>Durante a criação da interface percebi que era possível implementar diversas funcionalidades e regras de negócio, todas a nivel de front-end.</p>
-                        </div>
-                        <Link href="/react-bank">
-                            <a className={style.link}>Saiba mais</a>
-                        </Link>
-
-                    </div>
-                </article>
-
+                
             </section>
             <button className={style.readMore}>Ver todos</button>
         </div>
